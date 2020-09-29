c7902906fff92978c60d6a0b98083bc322725160
Thomas Hintermeier Saive <hintermeier-t@protonmail.com>
2020-09-29 16:13:03 +0200

    ajout de ressources

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                            gp_image=url_for('static', filename='img/hello.png'))
 
 @app.route('/about_me/')
 def about_me():
