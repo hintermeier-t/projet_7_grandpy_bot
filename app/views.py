25975f3c65b3dce9b37942a3ddb4694a39f48397
Thomas Hintermeier Saive <hintermeier-t@protonmail.com>
2020-09-29 14:15:24 +0200

    Mise en place du squelette du site, premiers essais avec flasks. Prochaine étape : les tests du parser

--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index/')
 def index():
-    return "Hello world !"
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/about_me/')
+def about_me():
+    return render_template('about_me.html')
+
+@app.route('/about_app/')
+def about_app():
+    return render_template('about_app.html')
