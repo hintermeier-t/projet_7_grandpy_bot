4bfa34369867d639e52af5e567fb58e276f57b43
Thomas Hintermeier Saive <hintermeier-t@protonmail.com>
2020-10-13 18:10:00 +0200

    Correction tests google maps

--- app/py_app/query.py
+++ app/py_app/query.py
@@ -28,22 +28,38 @@ class Query:
         #- Get the first address (from the most relevant place)
         self.address = place["candidates"][0]["formatted_address"]
         self.geocode = gmaps.geocode(self.address)
+        self.geocode = self.geocode[0]["geometry"]["location"]
 
     def wiki_query(self):
+        #- Format the geocode to request it
+        geocode_str = (str(self.geocode["lat"]) +
+            "|"+
+            str(self.geocode["lng"]))
         url = "https://fr.wikipedia.org/w/api.php"
-        address = Parser(self.formatted_address)
-        url = "https://fr.wikipedia.org/w/api.php"
+        #- The first wikipedia request to find a page near the location
         page_params = {
-                            "action": "query",
-                            "prop": "extracts",
-                            "explaintext": 1,
-                            "titles": address.string,
-                            "format": "json"
+            "action": "query",
+            "list": "geosearch",
+            "gscoord": geocode_str,
+            "gsradius": 1000,
+            "gslimit": 10,
+            "format": "json"
         }
+        page_request = requests.get(url, page_params)
+
+        #- Get the page id for the second request
+        page_id = str(page_request.json()["query"]["geosearch"][0]["pageid"])
         extract_params = {
-                        "action": "query",
-                        "prop": "extracts",
-                        "explaintext": 1,
-                        "titles": address.string,
-                        "format": "json"
+            "action": "query",
+            "prop": "extracts",
+            "explaintext": 1,
+            "pageids": page_id,
+            "format": "json"
         }
+        extract_request = requests.get(url, extract_params)
+
+        #- Gathering the extract and the page link for GrandPy's answer
+        self.extract = (extract_request.json()['query']['pages']
+            [page_id]['extract'])
+        self.link = ('https://fr.wikipedia.org/wiki/'+str(extract_request.json()
+            ['query']['pages'][page_id]['title']).replace(' ','_'))
--- app/tests/test_query.py
+++ app/tests/test_query.py
@@ -1,10 +1,17 @@
 """
     Script that tests if a query is sent and if a response is get.
 """
-
+from ..py_app.query import Query
+import requests
 #- Query test class
 class TestQuery:
+    def test_maps_response (monkeypatch):
+        address = '10 Quai de la Charente, 75019 Paris, France'
+        geocode = {'lat': 48.8974305, 'lng': 2.3835276}
 
+        def mockreturn(requests):
+            return address, geocode
+        monkeypatch.setattr(requests, 'get',mockreturn)
 
 #- Google Maps locate the city
 
