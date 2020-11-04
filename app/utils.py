e3551e4f3ac377a505ea612617676c41e1c48a6c
Thomas Hintermeier <hintermeier-t@protonmail.com>
2020-11-04 09:58:43 +0100

    MàJ stop_words.txt 2; corrections html, nettoyage utils.py

--- app/utils.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, url_for, request
-
-def log_writer(text):
-    text = request.args.get('u_message')
-    with open("app/templates/log.html", "a") as file:
-        file.write("<div class=\"messageLine\">"+text+"</div>")
-        file.close()
